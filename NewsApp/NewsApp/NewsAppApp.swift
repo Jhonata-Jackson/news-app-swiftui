49273c21b3465b3ccfd011a35f4c775f35554d54
Jhonata-Jackson <jhonata.jacksondev@gmail.com>
2025-01-14 15:21:18 -0300

    feat: :sparkles: add initial project files and configurations for NewsApp

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Jhonata Jackson on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
