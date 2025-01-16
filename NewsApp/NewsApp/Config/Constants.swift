4bc9ffc7706baea0823bd5d0c0e8f071c0b282e8
Jhonata-Jackson <jhonata.jacksondev@gmail.com>
2025-01-16 13:20:26 -0300

    refactor: :recycle: add constants for external links

---
+++ NewsApp/NewsApp/Config/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NewsApp
+//
+//  Created by Jhonata Jackson on 16/01/25.
+//
+
+import Foundation
+
+enum Constants {
+    static let twitter = "https://x.com/"
+    static let email =  "mailto:newsapp@gmail.com"
+    static let phone = "tel:21000000000"
+}
--- NewsApp/NewsApp/Views/SettingsView.swift
+++ NewsApp/NewsApp/Views/SettingsView.swift
@@ -31,10 +31,20 @@ struct SettingsView: View {
                 }
                 
                 Section {
-                    Label("Follow me on twitter @AnotherDevBr", systemImage: "link")
-                        .foregroundColor(.black)
-                        .font(.system(size: 14, weight: .semibold))
+                    Link(destination: URL(string: Constants.twitter)!,
+                         label: {
+                            Label("Follow me on twitter @AnotherDevBr", systemImage: "link")
+                    })
+                    
+                    Link("Contact me via email",
+                         destination: URL(string: Constants.email)!)
+                    
+                    Link("Call me",
+                         destination: URL(string: Constants.phone)!)
+                    
                 }
+                .foregroundColor(.black)
+                .font(.system(size: 14, weight: .semibold))
             }
             .navigationTitle("Settings")
         }
